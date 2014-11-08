d241bccf89732505cb9cce51ed6b6c0aa2ae1ed9
Kaizen <imgtn159@gmail.com>
2014-11-08 11:33:05 -0500

    Test Files

---
+++ demo.java
@@ -0,0 +1,12 @@
+public class demo{
+    public static void main(String[]args){
+	coordinate a,b,c;
+	a = new coordinate();
+	b = new coordinate(3,5);
+	c = b;
+	System.out.println("blah:"+a);
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);	
+    }
+}
