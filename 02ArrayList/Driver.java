d1cde96b83714ac17cf98c98434e93f2a5c27702
Kaizen <imgtn159@gmail.com>
2014-11-12 20:47:35 -0500

    Done

--- 02ArrayList/Driver.java
+++ 02ArrayList/Driver.java
@@ -4,20 +4,15 @@ public class Driver{
 	public static void collapseDuplicates(ArrayList<Integer> L){
 		int counter = 0;
 		ArrayList<Integer> indeces = new ArrayList<Integer>(10);
-		for (int q = 0;q < L.size();q++){
-			System.out.println("-----");
-			System.out.println(L.get(q));
-			System.out.println(counter);
-			System.out.println(L.get(q) == counter);
-			if (L.get(q) == counter){
+		for (int q:L){
+			if (q == counter){
 				indeces.add(q);
 			}else{
-				counter = L.get(q);
+				counter = q;
 			}
-			System.out.println(counter);
 		}
 		for (Integer q:indeces){
-			L.remove(L.get(q));
+			L.remove(q);
 		}
 	}
 	public static void main(String[]args){
