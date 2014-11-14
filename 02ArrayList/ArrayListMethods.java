c6c6fcd6372ed9b66bb871e939561701225edaf6
Kaizen <imgtn159@gmail.com>
2014-11-13 19:38:25 -0500

    Renamed files, added randomize function

--- 02ArrayList/ArrayListMethods.java
+++ 02ArrayList/ArrayListMethods.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Driver{
+public class ArrayListMethods{
 	public static void collapseDuplicates(ArrayList<Integer> L){
 		int counter = 0;
 		ArrayList<Integer> indeces = new ArrayList<Integer>(10);
@@ -15,6 +15,16 @@ public class Driver{
 			L.remove(q);
 		}
 	}
+
+	public static void randomize(ArrayList<Integer> L){
+		Random rand = new Random();
+		for (int q = L.size();q > 0;q--){
+			int i = rand.nextInt(q);
+			L.add(L.get(i));
+			L.remove(i);
+		}
+	}
+
 	public static void main(String[]args){
 		ArrayList<Integer> a = new ArrayList<Integer>(5);
 		a.add(new Integer(1));
@@ -24,6 +34,11 @@ public class Driver{
 		a.add(new Integer(2));
 		System.out.println(a);
 		collapseDuplicates(a);
+		System.out.println(a);
+		a.add(new Integer(3));
+		a.add(new Integer(4));
+		System.out.println(a);
+		randomize(a);
 		System.out.println(a);
 	}
 }
