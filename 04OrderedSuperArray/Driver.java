c89f5c93a02f8172fa22d6eced29185870f83a38
imgntn159 <imgtn159@gmail.com>
2014-12-01 12:00:37 -0500

    Adding super array folder

---
+++ 04OrderedSuperArray/Driver.java
@@ -0,0 +1,22 @@
+public class Driver{
+	public static void main(String[]args){
+		SuperArray a = new SuperArray(1);
+		a.add(new Integer(1));
+		a.add(new String("Two"));
+		a.add(new Integer(3));
+		System.out.println(a);
+		System.out.println(a.size());
+		a.resize(1);
+		System.out.println(a);
+		a.add(new String("Two"));
+		System.out.println(a);
+		System.out.println(a.get(2));
+		System.out.println(a.get(0));
+		a.resize(4);
+		System.out.println(a);
+		a.add(2,new String("Whatisthis"));
+		System.out.println(a);
+		a.remove(1);
+		System.out.println(a);
+	}
+}
---
+++ 04OrderedSuperArray/SuperArray.java
@@ -0,0 +1,109 @@
+public class SuperArray{
+	private Object[] array;
+	private int truelength = 0;
+	//constructors
+	public SuperArray(){
+		array = new Object[10];
+	}
+
+	public SuperArray(int x){
+		array = new Object[x];
+	}
+	//toString
+	public String toString(){
+		String result = "[ ";
+		for (Object e : array){
+			if (e != null){//will not print null objects
+				result = result + e + " ";
+			}
+		}
+		return result + "]";
+	}
+	//Getters
+	public int size(){
+		return truelength;
+	}
+
+	public Object get(int index){
+		if (index < 0  || index >= array.length - 1){
+			System.out.println("Error: Index out of range");
+			return null;
+		}
+		return array[index];
+	}
+	//Mutators
+	public Object set(int index, Object o){
+		if (index < 0  || index >= size()){
+			System.out.println("Error: Index out of range");
+			return null;
+		}
+		Object result = array[index]; 
+		array[index] = o;
+		return result;
+	}
+
+	public void add(Object e){
+		truelength++;
+		for (int i = 0;i < array.length;i++){
+			if(array[i] == null){
+				array[i] = e;
+				return;
+			}
+		}
+		resize(array.length + 1);
+		array[array.length - 1] = e;
+	}
+
+	public void add(int index, Object e){
+		if (truelength == array.length){
+			resize(array.length + 1);
+		}
+		if (index > array.length){
+			System.out.println("Error: Index out of range");
+			return;
+		}
+		Object[] temparray = new Object[array.length];
+		System.arraycopy(array, 0, temparray, 0, index+1);
+		for (int i = index;i < array.length-1;i++){
+			temparray[i+1] = array[i];
+		}
+		array = temparray;
+		array[index] = e;
+	}
+
+	public void remove(int index){
+		resize(truelength);
+		array[index] = null;
+		Object[] temparray = new Object[array.length];
+		boolean passednull = false;
+		for (int i = 0;i < array.length;i++){
+			if (array[i] == null){
+				passednull = true;
+			}
+			if (passednull){
+				temparray[i-1] = array[i];
+			}else{
+				temparray[i] = array[i];
+			}
+		}
+	}
+
+	public void resize(int newsize){
+		Object[] temparray = new Object[newsize];
+		int smaller = array.length;
+		if (newsize < array.length){
+			smaller = newsize;
+		}
+		for (int i = 0;i < smaller;i++){
+			temparray[i] = array[i];
+		}
+		array = temparray;
+	}
+
+	public void clear(){
+		for (int i = 0;i<array.length;i++){
+			array[i] = null;
+		}
+		truelength = 0;
+	}
+}
