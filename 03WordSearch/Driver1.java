d9cf229644d1e4e67350105f3230f0e39a4f7e7b
Kaizen <imgtn159@gmail.com>
2014-12-02 18:59:57 -0500

    WordGrid save point, haven't tested tostring

--- 03WordSearch/Driver1.java
+++ 03WordSearch/Driver1.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 
 public class Driver1{
 	public static void main(String[]args) throws FileNotFoundException{
-		WordGrid w;
+		WordGrid1 w;
 		if (args.length < 2){
 			System.out.println("\nHow to use:");
 			System.out.println("java Driver [rows] [cols] [seed(optional) [answers(optional)]]");
@@ -13,13 +13,13 @@ public class Driver1{
 			return;
 		}
 		if (args.length == 2){
-			w = new WordGrid(args[0],args[1],"words.txt");
+			w = new WordGrid1(Integer.parseInt(args[0]),Integer.parseInt(args[1]),"words.txt");
 		}
 		if (args.length == 3){
-			w = new WordGrid(args[0],args[1],"words.txt",args[2],0);
+			w = new WordGrid1(Integer.parseInt(args[0]),Integer.parseInt(args[1]),"words.txt",Long.parseLong(args[2]),0);
 		}
 		if (args.length == 4){
-			w = new WordGrid(args[0],args[1],"words.txt",args[2],args[3]);
+			w = new WordGrid1(Integer.parseInt(args[0]),Integer.parseInt(args[1]),"words.txt",Long.parseLong(args[2]),Integer.parseInt(args[3]));
 		}
 	}
 }
--- 03WordSearch/WordGrid1.java
+++ 03WordSearch/WordGrid1.java
@@ -11,9 +11,10 @@ public class WordGrid1{
     private ArrayList wordbank = new ArrayList(0);
     private boolean finishit = true;
 
-    public WordGrid1(int rows,int cols,String filename){
+    public WordGrid1(int rows,int cols,String filename)throws FileNotFoundException{
         data = new char[rows][cols];
         bank = new File(filename);
+        input = new Scanner(bank);
         clear();
     }
     public WordGrid1(int rows,int cols,String filename,long randSeed,int answer) throws FileNotFoundException{
@@ -39,6 +40,8 @@ public class WordGrid1{
         if (finishit){
             finalize();
         }
+        wordload();
+        System.out.println(wordsInPuzzle());
         String result = "";
         for (int q = 0;q<data.length;q++){
             for (int p = 0;p<(data[q].length);p++){
@@ -56,16 +59,16 @@ public class WordGrid1{
         if (dx == 0 || dy == 0){
             return false;
         }
-        if (dx == 1 && word.length()+col !<= data[0].length){
+        if (!(dx == 1 && word.length()+col <= data[0].length)){
             return false;
         }
-        if (dy == 1 && word.length()+row !<= data.length){
+        if (!(dy == 1 && word.length()+row <= data.length)){
             return false;
         }
-        if (dx == -1 && word.length() !<= col){
+        if (!(dx == -1 && word.length() <= col)){
             return false;
         }
-        if (dy == -1 && word.length() !<= row){
+        if (!(dy == -1 && word.length() <= row)){
             return false;
         }
         if (!(data[row][col] == 0 || data[row][col] == word.charAt(0))){
@@ -80,12 +83,14 @@ public class WordGrid1{
             for (int q = 0;q<word.length();q++){
                 data[row+dy][col+dx] = word.charAt(q);
             }
+            return true;
         }
+        return false;
     }
 
     public String wordsInPuzzle(){
         String result = "";
-        for (String word : wordbank){
+        for (Object word : wordbank){
             result+=word;
             result+="\n";
         }
@@ -94,6 +99,18 @@ public class WordGrid1{
 
     public void setSeed(long seed){
         rand.setSeed(seed);
+    }
+
+    public void wordload(){
+        while (input.hasNextLine()){
+            String a = input.nextLine();
+            for(int i = 0;i<(rand.nextInt(9)+1);i++){
+                if(addWord(a,rand.nextInt(data.length),rand.nextInt(data[0].length),rand.nextInt(data.length),rand.nextInt(data[0].length))){
+                    wordbank.add(a);
+                    break;
+                }
+            }
+        }
     }
 
     public void finalize(){
