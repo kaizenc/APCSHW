a97dbf4046a3978cd57c644f6b1fef92a1a4b588
Kaizen <imgtn159@gmail.com>
2014-12-10 20:45:43 -0500

    Fixed null pointer exception
    also added a try/catch block

--- 05GUICtoF/Windows.java
+++ 05GUICtoF/Windows.java
@@ -12,37 +12,50 @@ public class Windows extends JFrame implements ActionListener{
 	private Container buttons;
 	public Windows(){
 		this.setTitle("C to F Converter!");
-		this.setSize(600,400);
+		this.setSize(300,150);
 		this.setLocation(100,100);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 
 		pane = this.getContentPane();
-		pane.setLayout(new FlowLayout());
+		pane.setLayout(new GridLayout(2,1));
 
 		b = new JButton("Convert");
 		l = new JLabel("Converter",null,JLabel.CENTER);
 		t = new JTextField(12);
+		clear = new JButton("Clear");
 
 		b.setActionCommand("convert");
 		b.addActionListener(this);
 		clear.setActionCommand("delete");
 		clear.addActionListener(this);
 
-		pane.add(l);
-		pane.add(b);
-		pane.add(t);
+		buttons = new Container();
+		buttons.setLayout(new FlowLayout());
+		buttons.add(clear);
+		buttons.add(b);
 
 		text = new Container();
 		text.setLayout(new FlowLayout());
 		text.add(l);
 		text.add(t);
+
+		pane.add(text);
+		pane.add(buttons);
 	}
 
-	public void actionPerformed(ActionEvent e){
+	public void actionPerformed(ActionEvent e) throws NumberFormatException{
+		double x;
 		String action = e.getActionCommand();
+		loop:
 		if (action.equals("convert")){
 			String s = t.getText();
+			try{
+				x = Double.parseDouble(s);
+			}catch (NumberFormatException except){
+				System.out.println("Not a number, silly");
+				break loop;
+			}
 			s = "" + (Double.parseDouble(s)*1.8+32);
 			t.setText(s);
 		}
