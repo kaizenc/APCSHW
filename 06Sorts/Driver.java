9b16016a7b142ec7d6e3831fbce836758a6286f8
Kaizen <imgtn159@gmail.com>
2014-12-16 19:14:52 -0500

    BubbleSort works!...just bubblesort

--- 06Sorts/Driver.java
+++ 06Sorts/Driver.java
@@ -1,21 +1,9 @@
+import java.util.Arrays;
 public class Driver{
 	public static void main(String[]args){
-		SuperArray a = new SuperArray(1);
-		a.add("apple");
-		a.add("donkey");
-		a.add("bronco");
-		a.add("clock");
-		System.out.println(a);
-		System.out.println(a.size());
-		a.insertionSort();
-		System.out.println(a);
-		SuperArray b = new SuperArray(1);
-		b.add("donkey");
-		b.add("clock");
-		b.add("apple");
-		b.add("bronco");
-		System.out.println(b);
-		b.selectionSort();
-		System.out.println(b);
+		int[] a = new int[]{3,6,0,39,4};
+		System.out.println(Arrays.toString(a));
+		Sorts.bubbleSort(a);
+		System.out.println(Arrays.toString(a));
 	}
 }
--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,50 +1,52 @@
 public class Sorts{
-    public static void set(int index, int o){
-	if (index < 0  || index >= size()){
-	    System.out.println("Error: Index out of range");
-	    return;
-	} 
-	array[index] = o;
-    }
+	public static void set(int[]array,int index, int o){
+		if (index < 0  || index >= array.length){
+			System.out.println("Error: Index out of range");
+			return;
+		} 
+		array[index] = o;
+	}
     //Sorting
-    public static void insertionSort(int[]array){
-	int truelength = array.length;
-	for(int i = 1;i<truelength;i++){
-	    int q = i;
-	    while(q > 0 && array[q]<array[q-1]){
-		set(q,array[q-1]);
-		q--;
-	    }
-	    set(q,temp);
-	}
-    }
-    public static void selectionSort(int[]array){
-	int truelength = array.length;
-	for (int i = 0;i<truelength;i++){
-	    int temp = array[i];
-	    int j = i;
-	    for (int q = i;q<truelength;q++){
-		if(array[i]>array[q]){
-		    temp = array[q];
-		    j=q;
+	public static void insertionSort(int[]array){
+		int truelength = array.length;
+		for(int i = 1;i<truelength;i++){
+			int q = i;
+			int temp = array[q];
+			while(q > 0 && array[q]<array[q-1]){
+				set(array,q,array[q-1]);
+				q--;
+			}
+			set(array,q,temp);
 		}
-	    }
-	    array[j] = array[i];
-	    array[i] = temp;
 	}
-    }
-    public static void bubbleSort(int[]array){
-	int truelength = array.length;
-	for(int i = truelength;i>truelength;i--){
-	    int temp = array[i];
-	    int j = i;
-	    for (int q = i;q<truelength;q++){
-		if(array[q]>array[q+1]){
-		    temp = array[q];
-		    array[q] = array[q+1];
-		    array[q+1] = temp;
+	public static void selectionSort(int[]array){
+		int truelength = array.length;
+		for (int i = 0;i<truelength;i++){
+			int temp = array[i];
+			int j = i;
+			for (int q = i;q<truelength;q++){
+				if(array[i]>array[q]){
+					temp = array[q];
+					j=q;
+				}
+			}
+			array[j] = array[i];
+			array[i] = temp;
 		}
-	    }
 	}
-    }
+	public static void bubbleSort(int[]array){
+		boolean test = true;
+		int temp = array[0];
+		while(test){
+			test = false;
+			for (int i = 0;i<array.length-1;i++){
+				if(array[i]>array[i+1]){
+					temp = array[i];
+					array[i] = array[i+1];
+					array[i+1] = temp;
+					test = true;
+				}
+			}
+		}
+	}
 }
